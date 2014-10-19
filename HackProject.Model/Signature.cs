adcfed74ad53ecb666c04600df7de653e38f4789
boco4ever <boco4ever@yahoo.com>
2014-10-19 10:35:02 +0200

    added gesture recon

--- HackProject.Model/Signature.cs
+++ HackProject.Model/Signature.cs
@@ -5,6 +5,17 @@ namespace HackProject.Model
 {
     public class Signature : List<Point>
     {
-        
+        public event AddEvent AddPointEvent;
+        public Point AddPoint(Point point)
+        {
+            Add(point);
+            if (AddPointEvent != null)
+            {
+                AddPointEvent(this,new AddEventArgs(point));
+            }
+            return point;
+        }
     }
+
+    
 }
--- HackProject.View/MainWindow.xaml.cs
+++ HackProject.View/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using ConvertVectorIntoImage;
 using HackProject.Model;
 
@@ -23,28 +24,50 @@ namespace HackProject.View
     public partial class MainWindow : Window
     {
         private SignatureReader _reader;
+        private GestureReader _gestureReader;
         public MainWindow()
         {
             InitializeComponent();
             _reader = new SignatureReader();
+            _gestureReader=new GestureReader();
+        }
+
+        private void GestureMade(object sender, GestureEventArgs args)
+        {
+            if (!Dispatcher.CheckAccess())
+            {
+                Dispatcher.Invoke(
+                        () => ListOfPositions.Items.Add(args.GestureName), DispatcherPriority.Normal);
+            }
+          //  ListOfPositions.Items.Add(args.GestureName);
         }
 
         private void StartRead(object sender, RoutedEventArgs e)
         {
-            _reader.WriteSignature();
+            //_reader.WriteSignature();
+            _gestureReader.Start(GestureMade);
         }
 
         private void StopRead(object sender, RoutedEventArgs e)
         {
-            var signature = _reader.GetReadSignature();
-            foreach (System.Drawing.Point point in signature)
-            {
-                ListOfPositions.Items.Add(point.X + " " + point.Y);
+            //var signature = _reader.GetReadSignature();
+            _gestureReader.Stop();
+            //var gestures = _reader.GetGestures();
+            //foreach (string s in gestures)
+            //{
+            //    ListOfPositions.Items.Add(s);
+            //  //  IImageConverter imgConverter=new ImageConverter();
+            //   // imgConverter.ConvertImage(signature);
+            //}
+        }
 
-            }
-            IImageConverter imageConverter = new ImageConverter();
-            imageConverter.ConvertImage(signature);
-           
+
+        private void StartTestRead(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void StopTestRead(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
